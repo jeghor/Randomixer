1d8ad2dcca44cef4186e5c8eed2cc32bf9192455
Егор Шкляревский <egor39546@gmail.com>
2022-12-12 18:17:00 +0300

    Add average, summary and split methods

--- app/src/main/java/com/example/randomixer/MainActivity.kt
+++ app/src/main/java/com/example/randomixer/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.randomixer
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mix_button = findViewById<Button>(R.id.mix_button)
+        val mixButton = findViewById<Button>(R.id.mix_button)
 
-        mix_button.setOnClickListener {
+        mixButton.setOnClickListener {
             val intent = Intent(this,SecondActivity::class.java)
+            val randomArray = IntArray(randomNumbOfElem())
+            var counter = 0
+            while (counter< randomArray.size){
+                randomArray[counter] = kotlin.random.Random.nextInt(500)
+                counter++
+            }
+            intent.putExtra("array",randomArray)
             startActivity(intent)
         }
     }
+}
+
+fun randomNumbOfElem(): Int {
+    var elCount = IntArray(kotlin.random.Random.nextInt(2,26))
+    while(elCount.size%2!= 0){
+        elCount = IntArray(kotlin.random.Random.nextInt(2,26))
+    }
+    return elCount.size
 }
--- app/src/main/java/com/example/randomixer/SecondActivity.kt
+++ app/src/main/java/com/example/randomixer/SecondActivity.kt
@@ -2,10 +2,49 @@ package com.example.randomixer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val averageTextView = findViewById<TextView>(R.id.average_text)
+        val sumTextView = findViewById<TextView>(R.id.sum_all_text)
+        val splitTextView = findViewById<TextView>(R.id.split_set_text)
+        val allNumbersTextView = findViewById<TextView>(R.id.all_numbers)
+        var numbersText = ""
+        var counter = 0
+        val array =intent.getIntArrayExtra("array") as IntArray
+        while (counter<array.size) {
+            numbersText += "${array[counter]} "
+            counter++
+        }
+
+        averageTextView.text = averageOfNumbers(array).toString()
+        sumTextView.text = summaryOfAll(array).toString()
+        splitTextView.text = splitByTwo(array).toString()
+        allNumbersTextView.text =  numbersText //array.contentToString()
     }
+}
+
+private fun averageOfNumbers(array: IntArray): Int{
+    var summary = 0
+    for (element in array){
+        summary+=element
+    }
+    return summary/array.size
+}
+
+private fun summaryOfAll(array: IntArray):Int{
+    var summary = 0
+    for (element in array) summary+=element
+    return summary
+}
+
+private fun splitByTwo(array: IntArray):Int{
+    var summary = 0
+    for (element in array) summary+=element
+    var residual = 0
+    for (element in array) residual-=element
+    return summary/residual
 }
